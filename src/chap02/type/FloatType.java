8d73e169f2175e2699f331e9c04d4fa03471d66a
dlwl0005 <dlrp008@naver.com>
2020-09-29 15:23:58 +0900

    ㅇㅇㅇㅇㅇㅇ

---
+++ src/chap02/type/FloatType.java
@@ -0,0 +1,13 @@
+package chap02.type;
+
+public class FloatType {
+
+	public static void main(String[] args) {
+		// float : 4byte (32bits)
+		float floatValue = 3.14f;
+		System.out.println(floatValue);
+		
+
+	}
+
+}
