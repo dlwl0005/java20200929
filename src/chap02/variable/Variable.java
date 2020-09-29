8a3642b2796e85c7a6538a157873b9b5c30f8169
dlwl0005 <dlrp008@naver.com>
2020-09-29 11:40:23 +0900

    first

---
+++ src/chap02/variable/Variable.java
@@ -0,0 +1,14 @@
+package chap02.variable;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		// java 변수를 선언할 때 타입을 결정
+		int myVar;
+		myVar = 3;
+		System.out.println(myVar);
+		int myVar2 = 5;
+	}
+
+}
