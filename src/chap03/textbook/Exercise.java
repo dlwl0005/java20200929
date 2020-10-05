d0e4c300c79b0f8dffd5de9d0198aeb8da449939
dlwl0005 <dlrp008@naver.com>
2020-10-05 17:06:24 +0900

    d

---
+++ src/chap03/textbook/Exercise.java
@@ -0,0 +1,21 @@
+package chap03.textbook;
+
+public class Exercise {
+	public static void main(String[] args) {
+		
+		int value = 356;
+		System.out.println(300 % value);
+		double x = 5.0;
+		double y = 0.0;
+		
+		double z = x % y;
+		
+		if(Double.isNaN(z)) {
+			System.out.println("나눌수 없음");
+		}else {
+			double result = z+ 10;
+			System.out.println("결과: " + result);
+		}
+	}
+
+}
