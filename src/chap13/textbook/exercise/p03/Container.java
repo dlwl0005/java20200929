b35886bb9815e13c19b199e8906e228d4b8fa1e8
dlwl0005 <dlrp008@naver.com>
2020-10-22 16:16:17 +0900

    111111

---
+++ src/chap13/textbook/exercise/p03/Container.java
@@ -0,0 +1,19 @@
+package chap13.textbook.exercise.p03;
+
+public class Container<K, V> {
+	private K key;
+	private V value;
+	
+	
+	public void setKey(K key) {this.key = key;}
+	public void setValue(V value) {this.value = value;}
+
+	public K getKey() {return key;}
+	public V getValue() {return value;}
+	public void set(K k, V v) {
+		this.key = k;
+		this.value = v;
+		
+	}
+
+}
